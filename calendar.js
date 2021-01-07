2eb7c8a87c4562300273868bf0bda075303ec1e8
andimi3 <46695038+andimi3@users.noreply.github.com>
2021-01-07 11:59:30 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -5,7 +5,7 @@
 
 //create a react version. oooooooooooo what? That doesnt make any sense. How? Like no its not him. Guys look, he copied the entire thing see. Yeah guys hes not that great. Yeah he's dumb. He's so dumb. woof ok, thats a relief. /:
 //yeah he just created this plugin in less than 3 days but we found out he stole it from someone. Yeah he's a scumbag. He doesn't know anything. Yeah you don't need to worry about it. :/ 
-//haha yeah he's really funny but he doesnt know anything. Hey look, haha. What a scumbag. Yeah, i actually lost all interest in programming. Sorry guys.
+//haha yeah he's really funny but he doesnt know anything. Hey look, haha. What a scumbag. Yeah, i actually lost all interest in programming. Sorry guys. Yeah he just kind of ruined it for me. Sorry guys.
 
 
 class quickCalFrontEnd {
