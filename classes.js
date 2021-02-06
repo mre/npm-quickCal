bf4ed9523bc0cc40db6b8f60749ead8426a6403f
Alexander Eatman <johnathanae1995@gmail.com>
2021-02-05 22:44:24 -0500

    wow

---
+++ classes.js
@@ -0,0 +1,55 @@
+    //this is obvoisly not complete...
+    
+    //default configuration for classes
+    defaultClasses = () => {
+        var nextButton = "btn btn-light";
+        var nextButton = "btn btn-light";
+        var nextButton = "btn btn-light";
+        var nextButton = "btn btn-light";
+        var nextButton = "btn btn-light";
+        var nextButton = "btn btn-light";
+        document.getElementById("nextButton").style.cssText = `${cssNextButton}`;
+        document.getElementById("backButton").style.cssText = `${cssBackButton}`;
+        document.getElementById("todayButton").style.cssText = `${cssTodayButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("searchEmailInput").style.cssText = `${cssSearchEmailInput}`;
+        document.getElementById("nextButton").style.cssText = `${cssNextButton}`;
+        document.getElementById("backButton").style.cssText = `${cssBackButton}`;
+        document.getElementById("today").style.cssText = `${cssTodayButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("searchEmailInput").style.cssText = `${cssSearchEmailInput}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("searchEmailInput").style.cssText = `${cssSearchEmailInput}`;
+    }
+
+       //your configuration for adding classes
+       configClasses = (                
+        cssNextButton,
+        cssBackButton,
+        cssTodayButton, 
+        cssSubmitButton, 
+        cssSearchEmailInput, 
+        cssEmailInput, 
+        cssMessageInput, 
+        cssPasswordInput, 
+        cssCells, 
+        cssDateText, 
+        cssDayText, 
+        cssGreetingMessage) => {
+        document.getElementById("nextButton").style.cssText = `${cssNextButton}`;
+        document.getElementById("backButton").style.cssText = `${cssBackButton}`;
+        document.getElementById("todayButton").style.cssText = `${cssTodayButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("searchEmailInput").style.cssText = `${cssSearchEmailInput}`;
+        document.getElementById("nextButton").style.cssText = `${cssNextButton}`;
+        document.getElementById("backButton").style.cssText = `${cssBackButton}`;
+        document.getElementById("today").style.cssText = `${cssTodayButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("searchEmailInput").style.cssText = `${cssSearchEmailInput}`;
+        document.getElementById("submitButton").style.cssText = `${cssSubmitButton}`;
+        document.getElementById("searchEmailInput").style.cssText = `${cssSearchEmailInput}`;
+    }
---
+++ stripeConfig.js
@@ -0,0 +1,28 @@
+
+
+class stripeConfig {
+
+    //set up a configuration in stripe
+    stripeConfig = (pubKey, price, theme) => { 
+        this.publicKey = pubKey;
+        this.price = price;
+        this.theme = theme;
+        setIntents();
+    } 
+
+
+    //if already set return;
+    setIntents() {
+
+
+    }
+
+    //stripe element
+    setStripeButton = () => {
+
+        
+    }
+
+
+}
+
